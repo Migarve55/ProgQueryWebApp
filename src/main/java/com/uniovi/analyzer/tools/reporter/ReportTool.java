ff53fca67bdef334bad9490f68701ef1f09b6b22
Miguel Garnacho Velez <migarve55@gmail.com>
2019-04-10 18:32:54 +0200

    First working prototype

--- src/main/java/com/uniovi/analyzer/tools/reporter/ReportTool.java
+++ src/main/java/com/uniovi/analyzer/tools/reporter/ReportTool.java
@@ -26,9 +26,7 @@ public class ReportTool {
 	public void loadQueriesFromFile(String filepath) throws IOException {
 		try (Stream<String> stream = Files.lines(Paths.get(filepath))) {
 			stream.forEach((line) -> {
-				String[] tokens = line.split("\t");
-				if (tokens.length == 3)
-					queries.add(new Query(tokens[0], tokens[1], tokens[2]));
+				queries.add(new Query(line, "", ""));
 			});
 		}
 	}
@@ -50,9 +48,13 @@ public class ReportTool {
 	
 	private CodeError getCodeErrorFromResult(Map<String,Object> result) {
 		CodeError error = new CodeError();
-		error.setFile("file1");
-		error.setLine((int) result.get("line"));
-		error.setColumn((int) result.get("column"));
+		error.setFile(result.get("file").toString());
+		Long line = (Long) result.get("line");
+		if (line != null)
+			error.setLine(line);
+		Long column = (Long) result.get("column");
+		if (column != null)
+			error.setColumn(column);
 		error.setLevel(result.get("level").toString());
 		error.setDescription(result.get("description").toString());
 		return error;
