f23b35d1345ed121a781ebeb50774582ed63706e
Miguel Garnacho Velez <migarve55@gmail.com>
2019-06-10 16:22:51 +0200

    Docker integration

--- installMvnPlugin.bat
+++
@@ -1,2 +0,0 @@
-cd src/main/resources/
-mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=plugin/ProgQuery.jar -DgroupId=es.uniovi.progQuery -DartifactId=progQuery -Dversion=0.0.1-SNAPSHOT -DgeneratePom=true
---
+++ launchDockerInstance.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+APP_NAME = proqQueryWebApp
+IMG_NAME = uo257431/prog_query_web_app
+
+echo "Stoping current instance"
+docker stop $APP_NAME
+echo "Launching instance"
+docker pull $IMG_NAME
+docker create --name $APP_NAME $IMG_NAME
+docker start --attach miHelloWorld $APP_NAME
