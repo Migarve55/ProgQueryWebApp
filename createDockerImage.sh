e1161e0fd93aa0ba469af808f12b73a56706da51
Miguel Garnacho Velez <migarve55@gmail.com>
2019-06-10 21:50:37 +0200

    Docker files fixed

--- createDockerImage.sh
+++ createDockerImage.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-docker login --username=yourhubusername --email=youremail@company.com
-docker push uo257431/prog_query_web_app
+DOCKER_REPO = uo257431/prog_query_web_app
+
+echo "Login..."
+docker login --username=uo257431 --email=uo257431@uniovi.es
+
+echo "Building and pushing"
+mvn -Dmaven.test.skip=true package
+docker build -t $DOCKER_REPO
+docker push $DOCKER_REPO
--- installMvnPlugin.sh
+++ installMvnPlugin.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-cd src/main/resources/
-mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=plugin/ProgQuery.jar -DgroupId=es.uniovi.progQuery -DartifactId=progQuery -Dversion=0.0.1-SNAPSHOT -DgeneratePom=true
+mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=ProgQuery.jar -DgroupId=es.uniovi.progQuery -DartifactId=progQuery -Dversion=0.0.1-SNAPSHOT -DgeneratePom=true
