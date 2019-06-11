6d9d945756562ed2caab8802c192fd6920d6dd1e
Miguel Garnacho Velez <migarve55@gmail.com>
2019-06-11 12:30:41 +0200

    Docker quick fixes

--- createDockerImage.sh
+++ createDockerImage.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-DOCKER_REPO=uo257431/prog_query_web_app
-
-echo "Login..."
-docker login --username=uo257431 --email=uo257431@uniovi.es
+IMAGE_NAME=pqWebApp
 
 echo "Building and pushing"
 mvn -Dmaven.test.skip=true package
-docker build -t $DOCKER_REPO
-docker push $DOCKER_REPO
+docker build -t $IMAGE_NAME .
