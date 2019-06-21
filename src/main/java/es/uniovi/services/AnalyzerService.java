935656db07530b81833021b63caeba6408735a20
Miguel Garnacho Velez <migarve55@gmail.com>
2019-06-21 15:50:52 +0200

    Fixed bug

--- src/main/java/es/uniovi/services/AnalyzerService.java
+++ src/main/java/es/uniovi/services/AnalyzerService.java
@@ -14,6 +14,7 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import es.uniovi.analyzer.tasks.AbstractAnalyzerCallable;
@@ -100,6 +101,7 @@ public class AnalyzerService {
 		}
 	}
 	
+	@Transactional
 	private void createReport(User user, String name, List<ProblemDto> problems) {
 		Result result = new Result();
 		result.setUser(user);
